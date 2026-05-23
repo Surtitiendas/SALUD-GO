3c747c966591b71d70c64ddde1fc132b2333a2b5
Esme <erivera5@estudiante.ibero.edu.co>
2026-05-23 16:36:16 -0500

    crear un post

--- demo-vscode/index.js
+++ demo-vscode/index.js
@@ -12,9 +12,23 @@ console.log(`server is running at http://Localhost:${port}`);
 
 // creacion del crud
 
-let Especialidad = [
+let MEDICOS = [
     {id: 1, titulo: `Medico general`},
     {id: 2, titulo: `Medico optometra`},
     {id: 3, titulo: `Medico otorriinolaringolo`},
 ];
+
+//crear rutas
+
+//GET- LISTAR LOS MEDICOS
+app.get(`/Api/Medicos/`,(req, res) => {
+  const {id} = req.params;
+  const {Medico} = medico.find(1 => 1.id === parseInt(req.params.id));
+  if (!Medico) {
+    return res.status(404).json({ message: `medico no encontrado` });
+  }
+  res.json(Medico);
+});
+
+
 
