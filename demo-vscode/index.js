4352ff06641a0f21e958f98e413469766a29e649
Esme <erivera5@estudiante.ibero.edu.co>
2026-05-24 16:37:49 -0500

    FRONTEND

--- demo-vscode/index.js
+++ demo-vscode/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('hello, world');
+    res.send('hello, bienvenid@');
 });
 app.listen (port, () => {
 console.log(`server is running at http://Localhost:${port}`);
@@ -21,15 +21,53 @@ let MEDICOS = [
 //crear rutas
 
 //GET- LISTAR LOS MEDICOS
-app.get(`/Api/Medicos/`,(req, res) => {
-  const {id} = req.params;
-  const {Medico} = medico.find(1 => 1.id === parseInt(req.params.id));
-  if (!Medico) {
-    return res.status(404).json({ message: `medico no encontrado` });
-  }
-  res.json(Medico);
+app.get('/Medicos',(req, res) => {
+    res.json(MEDICOS);
 });
-//post- crear una nueva especialidad
 
+    //POST -CREAR
+app.post('/MEDICOS', (req, res) => {
+const nuevomedico = {
+id: MEDICOS.length + 1,
+Especialidades: req.body.Especialidades
+};
+MEDICOS.push(nuevomedico);
+res.status(201).json(nuevomedico);
+});
+
+// GET - OBTENER UN MÉDICO POR ID
+app.get('/MEDICOS/:id', (req, res) => {
+    const medico = MEDICOS.find(m => m.id === parseInt(req.params.id));
+
+    if (!medico) {
+        return res.status(404).json({ message: 'Medico no encontrado' });
+    }
+
+    res.json(medico);
+});
+
+// PUT - ACTUALIZAR
+app.put('/api/MEDICOS/:id', (req, res) => {
+    const medico = MEDICOS.find(m => m.id === parseInt(req.params.id));
+
+    if (!medico) {
+        return res.status(404).json({ message: 'Medico no encontrado' });
+    }
+
+    medico.titulo = req.body.titulo || medico.titulo;
+
+    res.json(medico);
+});
+
+//DELETE - ELIMINAR
+app.delete('/api/MEDICOS/:id', (req, res) => {
+    const EspecialidadesIndex = MEDICOS.findIndex(m => m.id === parseInt(req.params.id));
+    if (EspecialidadesIndex === -1) {
+        return res.status(404).json({ message: `medico no encontrado` });
+    }
+
+const EspecialidadesEliminado = MEDICOS.splice(EspecialidadesIndex, 1);
+res.json(EspecialidadesEliminado[0]);
+});
 
 
